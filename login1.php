0f4c75a0c6f7715bdb1158fdb01fbcc4e8a5b76b
JavedKh11 <khanjaved0413@gmail.com>
2020-01-31 15:24:22 +0530

    login code

---
+++ login1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+  <head>
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script type="text/javascript"></script>
+  </head>
+
+  <body>
+
+    <div><h2> Login </h2></div>
+    <form method="POST" action="" name="form">
+      <div>
+        Name  <input type="text" name="fname" id="fname" value="" required>
+        Email <input type="text" name="email" id="email" value="" required>
+        <br><br>
+
+        <input class="submit" type="submit" name="submit" id="btn1">
+      </div>
+    </form>
+  </body>
+</html>
+    
+
+
